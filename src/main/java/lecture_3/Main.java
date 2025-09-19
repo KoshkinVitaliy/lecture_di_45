a9f2a6ee2feb548f7fa3b5a06c0cdca6e2137a61
KoshkinVitaliy <kosh98vitaliy@outlook.com>
2025-09-19 14:45:36 +0300

    teacher is god

--- src/main/java/lecture_3/Main.java
+++ src/main/java/lecture_3/Main.java
@@ -2,14 +2,6 @@ package lecture_3;
 
 public class Main {
     public static void main(String[] args) {
-        VacuumCleaner cleaner2 = new VacuumCleaner(
-                "ergerger",
-                "ergergerge",
-                100f,
-                15_500
-        );
-        Shop.addVacuumCleaner(cleaner2);
-        Shop.showVaccumCleaners();
-
+        App.startApp();
     }
 }
